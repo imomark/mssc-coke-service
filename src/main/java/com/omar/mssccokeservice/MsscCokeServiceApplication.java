ec7ca48914004bd16503a48fa9ecbf0e4a9c4c9c
imomark <khanomarkhalid@gmail.com>
2020-09-06 18:20:52 +0530

    initial project add. Closes #1

---
+++ src/main/java/com/omar/mssccokeservice/MsscCokeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omar.mssccokeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscCokeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscCokeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omar/mssccokeservice/MsscCokeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.mssccokeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscCokeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
